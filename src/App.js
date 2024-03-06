8d4d5716142d00d170b30aa2201befdac450c38e
Abdurkader Abdi <abdurkaderabdi@gmail.com>
2024-03-06 02:32:19 -0500

    Move xIsNext to fit logic

--- src/App.js
+++ src/App.js
@@ -77,10 +77,11 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const xIsNext = currentMove % 2 === 0;
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
 
   function handlePlay(nextSquares) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
